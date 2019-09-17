6e0c8e1f60dacdd0519f87a31c6a24e21ab2a870
Andrei Vaniukov <aovnkv@yahoo.com>
2019-09-17 14:04:05 +0300

    layout finish

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="App-header">
+        <nav className="nav">
+          <div className="nav-item"><button className="btn btn-login"><a href="/login" className="btn-link">Войти</a></button></div>
+          <div className="nav-item"><a href="/profile" className="nav-link">Профиль</a></div>
+          <div className="nav-item"><a href="/news" className="nav-link">Новости</a></div>
+        </nav>
+      </div>
+      <main className="App-main">
+        <div className="content">
+          <div className="content__header">Заголовок</div>
+          <div className="content__main"></div>
+        </div>
+      </main>
     </div>
   );
 }
