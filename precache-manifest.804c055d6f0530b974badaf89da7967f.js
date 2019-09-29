22597fd49a21709b58b494109e082a5bf997c2a8
Andrei Vaniukov <aovnkv@yahoo.com>
2019-09-29 19:44:39 +0300

    Updates

---
+++ precache-manifest.804c055d6f0530b974badaf89da7967f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521c3dcfaac869e1b8c6ded8af28fb60",
+    "url": "/mtso-ikt-test/index.html"
+  },
+  {
+    "revision": "0b982876828399941ab4",
+    "url": "/mtso-ikt-test/static/css/2.c5b05437.chunk.css"
+  },
+  {
+    "revision": "7c3039310b5e67713af2",
+    "url": "/mtso-ikt-test/static/css/main.63de5db6.chunk.css"
+  },
+  {
+    "revision": "0b982876828399941ab4",
+    "url": "/mtso-ikt-test/static/js/2.b819d0dd.chunk.js"
+  },
+  {
+    "revision": "7c3039310b5e67713af2",
+    "url": "/mtso-ikt-test/static/js/main.e740beb7.chunk.js"
+  },
+  {
+    "revision": "82318404218214237f6f",
+    "url": "/mtso-ikt-test/static/js/runtime~main.42df3c8f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/mtso-ikt-test/precache-manifest.804c055d6f0530b974badaf89da7967f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mtso-ikt-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
