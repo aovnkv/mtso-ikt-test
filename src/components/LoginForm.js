c4ad8e4ca418fe2c6ea7400e03356c74e2b2dd09
Andrei Vaniukov <aovnkv@yahoo.com>
2019-10-30 00:48:40 +0300

    add: valid params for auth success

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,22 +5,37 @@ import { authenticateUser } from '../store/actions';
 import './LoginForm.css';
 
 const LoginForm = ({ authenticateUser, isSubmitting }) => {
-  const [formValues, setFormValues] = useState({ email: '', password: '', showMessage: false, message: 'Вы ввели неверный логин или пароль' })
-  const handleChange = e => setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+    showMessage: false,
+    message: 'Вы ввели неверный логин или пароль'
+  });
+  const handleChange = e =>
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
   const userID = useContext(UserContext);
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const res = await authenticateUser(formValues);
+      const res = await authenticateUser(formValues); //валидные параметры доступа max@test.com / 12345
       if (res === 'wrong_email_or_password') {
-        setFormValues({...formValues, password: '', showMessage: true});
+        setFormValues({ ...formValues, password: '', showMessage: true });
       }
     } catch (error) {
-        console.log(error.message);
+      console.log(error.message);
     }
   };
-  const warning = formValues.showMessage && <div style={{ color: 'red' }} className="warning">{ formValues.message }</div>;
-  if (userID) return <div className="content"><h1>Вы уже вошли на сайт как {userID}</h1></div>;
+  const warning = formValues.showMessage && (
+    <div style={{ color: 'red' }} className="warning">
+      {formValues.message}
+    </div>
+  );
+  if (userID)
+    return (
+      <div className="content">
+        <h1>Вы уже вошли на сайт как {userID}</h1>
+      </div>
+    );
 
   return (
     <div className="content">
@@ -28,28 +43,40 @@ const LoginForm = ({ authenticateUser, isSubmitting }) => {
       <form onSubmit={handleSubmit}>
         <div>
           <input
-          onChange={handleChange}
-          id="email"
-          type="email"
-          name="email"
-          value={formValues.email}
-          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-          required>
-          </input>
+            onChange={handleChange}
+            id="email"
+            type="email"
+            name="email"
+            value={formValues.email}
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            required
+          ></input>
           <label htmlFor="email">Эл. почта</label>
         </div>
         <div>
-          <input onChange={ handleChange } id="password" type="password" name="password" value={formValues.password} required></input>
+          <input
+            onChange={handleChange}
+            id="password"
+            type="password"
+            name="password"
+            value={formValues.password}
+            required
+          ></input>
           <label htmlFor="password">Пароль</label>
         </div>
-        { warning }
-        <button type="submit" disabled={isSubmitting}>Отправить</button>
+        {warning}
+        <button type="submit" disabled={isSubmitting}>
+          Отправить
+        </button>
       </form>
     </div>
-  )
+  );
 };
 
 const mapDispatchToProps = { authenticateUser };
 const mapStateToProps = ({ loginReducer }) => loginReducer;
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginForm);
